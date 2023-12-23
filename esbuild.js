12f1129f69121976bbd35901c556647eae032297
Rami-Wohl <80408407+Rami-Wohl@users.noreply.github.com>
2023-12-23 23:53:00 +0100

    switch to cjs module

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,15 @@
 const esbuild = require("esbuild");
-
+const { nodeExternalsPlugin } = require("esbuild-node-externals");
 esbuild
 	.build({
-		entryPoints: ["src/index.ts"],
-		outdir: "dist",
+		entryPoints: ["./src/index.ts"],
+		outfile: "dist/index.js",
 		bundle: true,
-		sourcemap: true,
 		minify: true,
-		splitting: true,
-		format: "esm",
-		target: ["esnext"],
+		treeShaking: true,
+		platform: "node",
+		format: "cjs",
+		target: "node14",
+		plugins: [nodeExternalsPlugin()],
 	})
 	.catch(() => process.exit(1));
